9d97f7ea59cb5ffe6e333b0f47bf9637aa8e4a3f
sahar-tabbarah <sahartabbarah@gmail.com>
2026-08-13 17:22:14 +0300

    Create real estate module structure

---
+++ custom_addons/real_estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Real Estate Management',
+    'description': 'A real estate management module for Odoo.',
+    'author': 'Sahar Tabbarah',
+    'category': 'Real Estate',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
---
+++ custom_addons/real_estate/models/property.py
@@ -0,0 +1,4 @@
+from odoo import models, fields
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Real Estate Property'
